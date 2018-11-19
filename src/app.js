172dd95debd99845dbd35662d637a3619f87651b
kev <ktran0125gmail.com>
2018-11-19 16:28:11 -0500

    Second milestone progress

--- src/app.js
+++ src/app.js
@@ -1,39 +1,79 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+require('./db');
+const User = mongoose.model('User');
+const Exercise = mongoose.model('Exercise');
+const session = require('express-session');
+const path = require('path');
 const app = express();
 
 
-app.set('view engine', 'hbs);
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+  extended: false
+}));
+
+//home page is login page
+app.get('/', (req, res) => {
 
 
-
-app.get('/', (req,res) =>{
-
-
-	res.render('home', template);
-
-
-
-});
-app.get('/add', (req,res) =>{
-
-
-	res.render('add', template);
-
-
-
-});
-
-
-app.get('/progress', (req,res) =>{
-
-
-	res.render('progress', template);
+  res.render('login');
 
 
 
 });
+app.get('/add', (req, res) => {
 
 
+  res.render('list')
+
+});
+
+app.post('/add', (req, res) => {
 
 
-app.listen(3000);
+  new Exercise({
+    exercise: req.body.name,
+    reps: req.body.reps,
+    sets: req.body.sets,
+    weight: req.body.weight,
+    rpe: req.body.rpe,
+    date: req.body.date
+  }).save(function(err, exercises) {
+		if (err){
+			console.log(err);
+		}
+		else{
+			res.redirect('/');
+
+		}
+  });
+
+});
+
+
+app.get('/progress', (req, res) => {
+
+  Exercise.find(function(err, exercises, count) {
+    res.render('progress', {
+      exercises: exercises
+
+    });
+  });
+
+
+});
+
+
+app.listen(process.env.PORT||3000);
--- src/db.js
+++ src/db.js
@@ -1,18 +1,57 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const appSchema = new Schema({
-	user: {
-		name: String,
-		weight: Double,
-		height: Double,
-		avatar: {
-				level: Number,
-					}	}
-	exercise: String,
-	reps: Double,
-	sets: Double,
-	weight: Double,
-	rpe: Double,
-	date: {type: Date, default: Date.now},
+const UserSchema = new mongoose.Schema({
+
+  name: String,
+  weight: Number,
+  height: Number,
+  avatar: {
+    level: Number,
+  },
+
+  exercises: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Exercise"
+  }]
+
 });
+
+const ExerciseSchema = new mongoose.Schema({
+  name: String,
+  reps: Number,
+  sets: Number,
+  weight: Number,
+  rpe: Number,
+  date: {
+    type: Date,
+    default: Date.now
+  },
+});
+
+
+const User = mongoose.model('User', UserSchema);
+const Exercise = mongoose.model('Exercise', ExerciseSchema);
+
+
+// is the environment variable, NODE_ENV, set to PRODUCTION?
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+ // if we're in PRODUCTION mode, then read the configration from a file
+ // use blocking file io to do this...
+ const fs = require('fs');
+ const path = require('path');
+ const fn = path.join(__dirname, 'config.json');
+ const data = fs.readFileSync(fn);
+
+ // our configuration file will be in json, so parse it and set the
+ // conenction string appropriately!
+ const conf = JSON.parse(data);
+ dbconf = conf.dbconf;
+} else {
+ // if we're not in PRODUCTION mode, then use
+ dbconf = 'mongodb://localhost/YOUR_DATABASE_NAME_HERE';
+}
+
+//password is password for MongoDB from part 3
+mongoose.connect('dbconf');
