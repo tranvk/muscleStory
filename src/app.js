c9ea9955d196cb9b6971009cf7639a5931ed48a7
kev <ktran0125gmail.com>
2018-11-28 15:37:58 -0500

    Progress page works

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const User = mongoose.model('User');
 const Exercise = mongoose.model('Exercise');
 const session = require('express-session');
 const path = require('path');
+const auth = require('auth');
 
 const app = express();
 
@@ -18,7 +19,7 @@ app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({
-	extended: true
+  extended: true
 }));
 
 app.use(express.json());
@@ -36,6 +37,66 @@ app.get('/', (req, res) => {
 
 
 });
+
+app.get('/login', (req,res)) => {
+
+	res.render('login');
+});
+
+app.post('/login', (req, res) => {
+
+  //if login doesn't work, re-render login template with error
+  const errorCallback = function(message) {
+    res.render("login", messsage);
+
+  };
+
+  // if login works
+  const successCallback = function(user) {
+    auth.startAuthenticatedSession(req, user, function(err) {
+      if (err) {
+        res.redirect('/');
+      }
+
+    });
+  };
+
+  //call auth's login function with callbacks defined above
+  auth.login(req.body.username, req.body.password, errorCallback, successCallback);
+
+
+});
+
+app.get('/register', (req,res)) =>{
+	res.render('register');
+});
+
+app.post('/register', (req, res) => {
+
+
+
+  //error callback with various error messages
+  const errorCallback = function(message) {
+    res.render('register', message);
+  };
+
+  //only called if registration works
+  const successCallback = function(user) {
+    //assuming new user exists'
+    auth.startAuthenticatedSession(req, user, (err) => {
+      if (err) {
+        res.redirect('/'); //redirect to home page
+      }
+    });
+
+  };
+
+  //call auth's register function with callbacks defined above
+  auth.register(req.body.username, req.body.email, req.body.password, errorCallback, successCallback);
+
+});
+
+
 app.get('/add', (req, res) => {
 
 
@@ -43,24 +104,24 @@ app.get('/add', (req, res) => {
 
 });
 
+
 app.post('/add', (req, res) => {
 
 
   new Exercise({
-    exercise: req.body.name,
+    name: req.body.name,
     reps: req.body.reps,
     sets: req.body.sets,
     weight: req.body.weight,
     rpe: req.body.rpe,
     date: req.body.date
   }).save(function(err, exercises) {
-		if (err){
-			console.log(err);
-		}
-		else{
-			res.redirect('/');
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
 
-		}
+    }
   });
 
 });
@@ -69,6 +130,7 @@ app.post('/add', (req, res) => {
 app.get('/progress', (req, res) => {
 
   Exercise.find(function(err, exercises, count) {
+    console.log(exercises);
     res.render('progress', {
       exercises: exercises
 
@@ -79,4 +141,4 @@ app.get('/progress', (req, res) => {
 });
 
 
-app.listen(process.env.PORT||3000);
+app.listen(process.env.PORT || 3000);
--- src/db.js
+++ src/db.js
@@ -6,9 +6,7 @@ const UserSchema = new mongoose.Schema({
   name: String,
   weight: Number,
   height: Number,
-  avatar: {
-    level: Number,
-  },
+  level: Number,
 
   exercises: [{
     type: mongoose.Schema.Types.ObjectId,
@@ -25,7 +23,7 @@ const ExerciseSchema = new mongoose.Schema({
   rpe: Number,
   date: {
     type: Date,
-    default: Date.now
+    default: Date.now //activates when date field is undefined
   },
 });
 
@@ -37,21 +35,21 @@ const Exercise = mongoose.model('Exercise', ExerciseSchema);
 // is the environment variable, NODE_ENV, set to PRODUCTION?
 let dbconf;
 if (process.env.NODE_ENV === 'PRODUCTION') {
- // if we're in PRODUCTION mode, then read the configration from a file
- // use blocking file io to do this...
- const fs = require('fs');
- const path = require('path');
- const fn = path.join(__dirname, 'config.json');
- const data = fs.readFileSync(fn);
+  // if we're in PRODUCTION mode, then read the configration from a file
+  // use blocking file io to do this...
+  const fs = require('fs');
+  const path = require('path');
+  const fn = path.join(__dirname, 'config.json');
+  const data = fs.readFileSync(fn);
 
- // our configuration file will be in json, so parse it and set the
- // conenction string appropriately!
- const conf = JSON.parse(data);
- dbconf = conf.dbconf;
+  // our configuration file will be in json, so parse it and set the
+  // conenction string appropriately!
+  const conf = JSON.parse(data);
+  dbconf = conf.dbconf;
 } else {
- // if we're not in PRODUCTION mode, then use
- dbconf = 'mongodb://localhost/YOUR_DATABASE_NAME_HERE';
+  // if we're not in PRODUCTION mode, then use
+  dbconf = 'mongodb://localhost/musclestory';
 }
 
 //password is password for MongoDB from part 3
-mongoose.connect('dbconf');
+mongoose.connect(dbconf);
